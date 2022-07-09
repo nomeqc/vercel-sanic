297b6a1f746b4e971b2577aab62ed8e7b3fa1e3f
Nomeqc <nomeqc@gmail.com>
2022-07-10 01:46:06 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,12 +5,12 @@ from sanic_template import template as Template
 
 app = Sanic("api")
 template = Template(path=Path(__file__).parent.joinpath("templates"))
-
+print('外面')
 
 @app.route("/")
 async def html(request):
-    print(f'template: {template}')
-    vars = {'title': 'hello world', 'word': 'Tom'}
+    print(f'template: {template.path}')
+    vars = {'title': 'hello world', 'name': 'Tom'}
     return await template.render_template("test.html", **vars)
 
 if __name__ == '__main__':
